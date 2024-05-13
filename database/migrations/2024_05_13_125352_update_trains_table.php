097b7211266904e12fc765110a6f9cead7488a43
simoneaucello <simoneaucello@gmail.com>
2024-05-13 15:00:46 +0200

    [add] update migration

---
+++ database/migrations/2024_05_13_125352_update_trains_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+
+            $table->boolean('in_time')->default(true)->after('car_number');
+            $table->boolean('cancelled')->default(false)->after('car_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+
+            $table->dropColumn('in_time');
+            $table->dropColumn('cancelled');
+        });
+    }
+};
