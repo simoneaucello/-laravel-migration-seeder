9afcc37ab4b05e3dfa9aad253411cbcd65b5fe3f
simoneaucello <simoneaucello@gmail.com>
2024-05-13 14:53:11 +0200

    [add] trains table migration

---
+++ database/migrations/2024_05_13_123305_create_trains_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name', 100);
+            $table->string('slug', 100)->unique();
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->time('departure_time')->default('00:00');
+            $table->time('arrival_time')->default('00:00');
+            $table->string('train_code', 10)->unique();
+            $table->tinyInteger('car_number')->nullable()->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
