7185239a22786a46c28157a41b370891b4d8b0aa
simoneaucello <simoneaucello@gmail.com>
2024-05-14 15:23:26 +0200

    [add] faker data

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -16,8 +16,16 @@ class TrainsTableSeeder extends Seeder
     {
         for ($i = 0; $i < 50; $i++) {
             $new_train = new Train();
-            $new_train->company_name = $faker->words(1, true);
-            $new_train->departure_station = $faker->words(2, true);
+            $new_train->company_name = $faker->company();
+            $new_train->departure_station = $faker->city();
+            $new_train->arrival_station = $faker->city();
+            $new_train->departure_time = $faker->time('H:i');
+            $new_train->arrival_time = $faker->time('H:i');
+            $new_train->train_code = $faker->regexify('[A-Z]{5}[0-4]{3}');
+            $new_train->car_number = $faker->numberBetween(2, 15);
+            $new_train->in_time = $faker->boolean();
+
+            $new_train->save();
         }
     }
 }
