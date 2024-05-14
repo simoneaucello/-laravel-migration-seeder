f5bc01d989f8eca2f4488ffe41eccfaa7b23dd51
simoneaucello <simoneaucello@gmail.com>
2024-05-14 14:37:54 +0200

    [add] seeder + model

--- database/migrations/2024_05_13_123305_create_trains_table.php
+++ database/migrations/2024_05_13_123305_create_trains_table.php
@@ -14,12 +14,11 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->string('company_name', 100);
-            $table->string('slug', 100)->unique();
             $table->string('departure_station', 100);
             $table->string('arrival_station', 100);
             $table->time('departure_time')->default('00:00');
             $table->time('arrival_time')->default('00:00');
-            $table->string('train_code', 10)->unique();
+            $table->string('train_code', 12)->unique();
             $table->tinyInteger('car_number')->nullable()->unsigned();
             $table->timestamps();
         });
---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 50; $i++) {
+            $new_train = new Train();
+            $new_train->company_name = $faker->words(1, true);
+        }
+    }
+}
